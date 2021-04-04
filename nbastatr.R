b011b9a2a28753ccb6b7d24c62a2a1178ebdf675
joedemarchis <81887227+joedemarchis@users.noreply.github.com>
2021-04-03 20:57:48 -0400

    Create nbastatr.R
    This should be a working copy of the nbastatr package. I have filtered out the years before 1989 due to the fact that in 1989 the NBA switched over to the 2 round draft model.

---
+++ nbastatr.R
@@ -0,0 +1,11 @@
+devtools::install_github("abresler/nbastatR")
+library(nbastatR)
+if(!require(ggplot2)) install.packages("ggplot2")
+library(ggplot2)
+if(!require(dplyr)) install.packages("dplyr")
+library(dplyr)
+if(!require(plotly)) install.packages("plotly")
+library(plotly)
+df_drafts <-
+  drafts(draft_years = 1989:2018, nest_data = FALSE, return_message = TRUE)
+View(df_drafts)
