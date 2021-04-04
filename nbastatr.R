e98d79db536abea481f8635b9ff07b7ce6d78326
joedemarchis <81887227+joedemarchis@users.noreply.github.com>
2021-04-03 22:00:21 -0400

    Update nbastatr.R
    Removed-
    old way of getting player totals
    added-
    new way of getting player totals(quicker)
    need to add-
    combining df_drafts and player_stats
    removing players who do not appear on df_drafts

--- nbastatr.R
+++ nbastatr.R
@@ -6,11 +6,10 @@ if(!require(dplyr)) install.packages("dplyr")
 library(dplyr)
 if(!require(plotly)) install.packages("plotly")
 library(plotly)
+#draft pos for players from 1989-2018
 df_drafts <-
   drafts(draft_years = 1989:2018, nest_data = FALSE, return_message = TRUE)
 View(df_drafts)
-#trying to figure out how to get a list of all the player names from the df_drafts table inserted where
-#"Joe Harris", "Myles Turner", etc.. is on the line below. 
-totals <- players_careers(players = c("Joe Harris", "Myles Turner", "Spencer Dinwiddie"),
-                modes = c("Totals", "PerGame"))
-
+#player stats from 1989-2018
+player_stats<-bref_players_stats(seasons = 1989:2018, tables = c("totals"))
+View(player_stats)
