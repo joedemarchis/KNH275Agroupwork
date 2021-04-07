a6e9a9e953c05cf9ff90714833b93c3015d1c08c
joedemarchis <81887227+joedemarchis@users.noreply.github.com>
2021-04-07 19:24:29 -0400

    Update nbastatr.R
    got the csv download, cleaned up code.

--- nbastatr.R
+++ nbastatr.R
@@ -1,14 +1,12 @@
-#line 2 may not be needed
-devtools::install_github("abresler/nbastatR")
-library(nbastatR)
-if(!require(ggplot2)) install.packages("ggplot2")
-library(ggplot2)
-if(!require(dplyr)) install.packages("dplyr")
-library(dplyr)
-if(!require(plotly)) install.packages("plotly")
-library(plotly)
-install.packages("xlsx")
-library(xlsx)
+  #line 2 may not be needed
+  devtools::install_github("abresler/nbastatR")
+  library(nbastatR)
+  if(!require(ggplot2)) install.packages("ggplot2")
+  library(ggplot2)
+  if(!require(dplyr)) install.packages("dplyr")
+  library(dplyr)
+  if(!require(plotly)) install.packages("plotly")
+  library(plotly)
 #draft position for players from 1989-2018
 df_drafts <-
   drafts(draft_years = 1989:2018, nest_data = FALSE, return_message = TRUE)
@@ -19,10 +17,8 @@ View(player_stats)
 #filtering out players not drafted between 1989-2018
 drafted_player_complete_stats <- inner_join(df_drafts,player_stats, by = "namePlayer")  
 View(drafted_player_complete_stats)
+#filtering stats we need
+complete_stats<-select(drafted_player_complete_stats,yearDraft,numberPickOverall,numberRound,numberRoundPick,namePlayer,nameTeam,slugTeamBREF,slugSeason,slugPosition,agePlayer,countGames,pctFG,pctFG3,pctFG2,pctEFG,pctFT,ftmTotals,trbTotals,astTotals,stlTotals,blkTotals,tovTotals,pfTotals,ptsTotals)
+View(complete_stats)
 #saving as excel
-write.xlsx(x = df_drafts,                       
-           file = "draftdata.xlsx")
-write.xlsx(x = player_stats,                       
-           file = "playerstats.xlsx")
-write.xlsx(x = drafted_player_complete_stats,                       
-           file = "complete_stats.xlsx")
+write.csv(complete_stats, "draftedstats.csv")
